bc9ac4dcec7a51d36fb85ca93689560b5941229d
20Lush <zachfsx83@gmail.com>
2023-05-13 02:03:42 -0700

    added flavor text, license

--- src/dynamixel_sync_read_write.cpp
+++ src/dynamixel_sync_read_write.cpp
@@ -1,3 +1,31 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//    _   _   _   _   _   _   _   _   _   _   _   _   _   _   _   _
+// ,-(_)-(_)-(_)-(_)-(_)-(_)-(_)-(_)-(_)-(_)-(_)-(_)-(_)-(_)-(_)-(_)
+// `-' `-' `-' `-' `-' `-' `-' `-' `-' `-' `-' `-' `-' `-' `-' `-'
+// 		        __      __  __  ______  __  __    
+// 	(•̀ᴗ•́)و	 /\ \    /\ \/\ \/\  ___\/\ \_\ \    (◍＞◡＜◍)
+// 	 ᶘ ◕ᴥ◕ᶅ	  \ \ \___\ \ \_\ \ \___  \ \  __ \  【≽ܫ≼】
+// 	(ﾟ◥益◤ﾟ)   \ \_____\ \_____\/\_____\ \_\ \_\  (ʘ言ʘ╬)
+// 	 ᕙ(⇀‸↼‶)ᕗ  \/_____/\/_____/\/_____/\/_/\/_/  (◕‿◕✿)
+//    _   _   _   _   _   _   _   _   _   _   _   _   _   _   _   _
+// ,-(_)-(_)-(_)-(_)-(_)-(_)-(_)-(_)-(_)-(_)-(_)-(_)-(_)-(_)-(_)-(_)
+// `-' `-' `-' `-' `-' `-' `-' `-' `-' `-' `-' `-' `-' `-' `-' `-'
+//
+
+// This node implementation is derived from ROBOTIS CO., LTD. example code "read_write_node"
+// Example Code Author: Will Son
+
 #include <memory>
 #include <string>
 
