3f84ce0bf90520967fc9624245609454cfb56af0
20Lush <zachfsx83@gmail.com>
2023-05-18 18:15:15 -0700

    map hack

--- src/joint_server.cpp
+++ src/joint_server.cpp
@@ -49,7 +49,7 @@
 #define PRECISION_OFFSET -20
 
 int map(float x, float in_min, float in_max, int out_min, int out_max) {
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min; 
+  return x; //(x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min; 
 }
 
 JointServer::JointServer() : Node ("joint_server") {
